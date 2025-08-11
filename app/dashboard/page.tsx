cce7c1d891f4f67a93657103cb5be63407da3302
MDavidka <117516603+MDavidka@users.noreply.github.com>
2025-08-11 16:21:45 +0300

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -206,7 +206,7 @@ export default function Dashboard() {
                         backgroundImage: server.serverIcon
                           ? `url(https://cdn.discordapp.com/icons/${server.serverId}/${server.serverIcon}.png?size=128)`
                           : "none",
-                        backgroundSize: "460%",
+                        backgroundSize: "690%",
                         backgroundPosition: "center",
                       }}
                     ></div>
